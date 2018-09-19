48a7f02f21b9b7adaea243ba3aea2e27f0189d02
Anton Kuznetsov <isnifer@gmail.com>
2018-09-19 12:12:35 +0300

    Update prometheus/index.js

--- prometheus/index.js
+++ prometheus/index.js
@@ -1,8 +1,19 @@
 import * as helpers from '../helpers'
 
-const Prometheus = process.env.INSTRUMENT === 'detox'
-  ? require('./PrometheusDetox').default
-  : require('./PrometheusAppium').default
+const { INSTRUMENT } = process.env
+
+if (!INSTRUMENT) {
+  throw new Error('You should define an "INSTRUMENT" environment variable')
+}
+
+const instruments = {
+  detox: require('./PrometheusDetox').default,
+  appium: require('./PrometheusAppium').default,
+  enzyme: require('./PrometheusEnzyme').default,
+  web: require('./PrometheusWeb').default,
+}
+
+const Prometheus = instruments[INSTRUMENT]
 
 const prometheus = new Prometheus()
 
